b0107de5610b34d294c80c5d2de426a9c7b600f2
Matias <pinkystylee98@gmail.com>
2019-10-09 16:13:21 -0300

    Creacion Métodos de Controlador de Organizadores, entre otros.
    Se crearon los metodos del controlador de organizador, ademas de los cascarones de los metodos de gestion de eventos, ya que la clase eventos aún no esta implementada.

--- EvenTinder/src/ControladorUsuarios/ControladorOrganizador.java
+++ EvenTinder/src/ControladorUsuarios/ControladorOrganizador.java
@@ -1,97 +1,114 @@
 package ControladorUsuarios;
 
+import ModuloAutenticacion.ControlDeAcceso;
 import ModuloGestionEventos.Evento;
-import java.util.*;
+import ModuloGestionEventos.GestionDeEvento;
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * 
  */
 public class ControladorOrganizador {
+    
+    private ControlDeAcceso controlAcceso;
+    private GestionDeEvento gestorEventos;
 
     /**
      * Default constructor
      */
     public ControladorOrganizador() {
+        this.controlAcceso = ControlDeAcceso.getIntancia();
+        this.gestorEventos = new GestionDeEvento();
     }
 
-
-
     /**
-     * @param String nombre 
-     * @param String descripcion 
-     * @param Date fechaDeInicio 
-     * @param Date fechaDeTermino 
-     * @param int capacidad 
-     * @param int diasMaximoDevolucion 
-     * @param boolean publicado 
-     * @param int idPropiedad 
+     * Metodo que crea un nuevo Evento asociado al Organizador correspondiente.
+     * 
+     * @param nombre El nombre del evento a crear.
+     * @param descripcion La descripcion asociada al nuevo evento.
+     * @param fechaDeInicio La fecha de Inicio del Evento.
+     * @param fechaDeTermino La fecha de Termino del Evento
+     * @param capacidad La capacidad maxima de asistentes del Evento.
+     * @param diasMaximoDevolucion Los dias maximos que tiene un Cliente despues 
+     * @param publicado 
+     * @param idPropiedad El ID de la propiedad a la cual estara asociado el evento.
      * @return
      */
-    public boolean crearEvento(String nombre, String descripcion, Date fechaDeInicio, Date fechaDeTermino, int capacidad, int diasMaximoDevolucion, boolean publicado, int idPropiedad) {
-        // TODO implement here
-        return false;
+    public boolean crearEvento(String nombre, String descripcion, Date fechaDeInicio, Date fechaDeTermino, int capacidad, int diasMaximoDevolucion, boolean publicado, int idPropiedad) {       
+        boolean result = this.gestorEventos.crearEvento(nombre, descripcion, fechaDeInicio, fechaDeTermino, capacidad, diasMaximoDevolucion, publicado, idPropiedad);
+        return result;
     }
 
     /**
-     * @param int idEvento 
-     * @param String nombre 
-     * @param String descripcion 
-     * @param Date fechaDeInicio 
-     * @param Date fechaDeTermino 
-     * @param int capacidad 
-     * @param int diasMaximoDevolucion 
-     * @param boolean publicado 
-     * @param int idPropiedad 
+     * Metodo que permite modificar los parametros de un Evento especifico, que pertenezca al Organizador 
+     * que esta ingresado en el sistema actualmente.
+     * 
+     * @param idEvento El ID del evento que se desea modificar.
+     * @param nombre El nuevo nombre del Evento a modificar.
+     * @param descripcion La nueva descripcion del Evento a modificar.
+     * @param fechaDeInicio La nueva fecha de Inicio del Evento a modificar.
+     * @param fechaDeTermino La neuva fecha de Termino del Evento a modificar.
+     * @param capacidad La nueva capacidad maxima de asistentes del Evento a modificar.
+     * @param diasMaximoDevolucion 
+     * @param publicado 
+     * @param idPropiedad 
      * @return
      */
     public boolean modificarEvento( int idEvento,  String nombre, String descripcion, Date fechaDeInicio, Date fechaDeTermino, int capacidad, int diasMaximoDevolucion, boolean publicado, int idPropiedad) {
-        // TODO implement here
-        return false;
+        return this.gestorEventos.modificarEvento(idEvento, nombre, descripcion, fechaDeInicio, fechaDeTermino, capacidad, diasMaximoDevolucion, publicado, idPropiedad);
     }
 
     /**
-     * @param int idEvento 
-     * @return
+     * Metodo que permite Eliminar un Evento del sistema, que este asociado al Organizador que esta actualmente
+     *   ingresado en el sistema.
+     * @param idEvento EL ID del evento que se desea eliminar.
+     * @return True si el Evento fue eliminado con exito. False si el Evento no pudo eliminarse.
      */
-    public boolean eliminarEvento(int idEvento) {
-        // TODO implement here
-        return false;
+    public boolean eliminarEvento(int idEvento) {        
+        return this.gestorEventos.eliminarEvento(idEvento);
     }
 
     /**
-     * @return
+     * Metodo que retorna todos los Eventos que estan asociados al Organizador actualmente ingresado en el sistema.
+     * 
+     * @return El arreglo con todos los Eventos que estan asociados al Organizador actualmente ingresado en el sistema.
      */
      public ArrayList<Evento> obtenerInformacionDeTodosLosEventosDeUnOrganizador() {
-        // TODO implement here
-        return null;
+        return this.gestorEventos.obtenerInformacion(this.controlAcceso.getRut(), "Todos");       
     }
 
     /**
-     * @return
+     * Metodo que retorna todos los Eventos Publicados que estan asociados al Organizador actualmente ingresado en el sistema.
+     * 
+     * @return El arreglo con todos los Eventos Publicados que estan asociados al Organizador actualmente ingresado en el sistema.
      */
     public ArrayList<Evento> obtenerInformacionDeEventosPublicadosDeUnOrganizador() {
-        // TODO implement here
-        return null;
+        return this.gestorEventos.obtenerInformacion(this.controlAcceso.getRut(), "Publicados");
     }
 
     /**
-     * @return
+     * Metodo que retorna todos los Eventos No Publicados que estan asociados al Organizador actualmente ingresado en el sistema.
+     * 
+     * @return El arreglo con todos los Eventos No Publicados que estan asociados al Organizador actualmente ingresado en el sistema.
      */
     public ArrayList<Evento> obtenerInformacionDeEventosNoPublicadosDeUnOrganizador() {
-        // TODO implement here
-        return null;
+        return this.gestorEventos.obtenerInformacion(this.controlAcceso.getRut(),"No Publicados");
     }
 
     /**
-     * @return
+     * Metodo que retorna todos los Eventos Finalizados que estan asociados al Organizador actualmente ingresado en el sistema.
+     * 
+     * @return El arreglo con todos los Eventos Finalizados que estan asociados al Organizador actualmente ingresado en el sistema.
      */
     public ArrayList<Evento> obtenerInformacionDeEventosFinalizadosDeUnOrganizador() {
-        // TODO implement here
-        return null;
+        return this.gestorEventos.obtenerInformacion(this.controlAcceso.getRut(),"Finalizados");
     }
 
     /**
-     * @param int id 
+     * Iteracion 4 
+     * Proximamente
+     * @param id 
      * @return
      */
     public ArrayList<String> obtenerInformacionDeEstadisticasDeVentasPorEventos(int id) {
--- EvenTinder/src/ModuloGestionEventos/GestionDeEvento.java
+++ EvenTinder/src/ModuloGestionEventos/GestionDeEvento.java
@@ -1,81 +1,106 @@
 package ModuloGestionEventos;
 
-import java.util.*;
+import ControladorBaseDeDatos.ControladorBDDeEventos;
+import java.util.ArrayList;
+import java.util.Date;
+
 
 /**
  * 
  */
 public class GestionDeEvento {
+    
+    private ControladorBDDeEventos controlador;
+    private ArrayList<Evento> listaEventos;
 
     /**
      * Default constructor
      */
     public GestionDeEvento() {
+        this.controlador = new ControladorBDDeEventos();
     }
 
     /**
-     * @param String nombre 
-     * @param String descripcion 
-     * @param Date fechaDeInicio 
-     * @param Date fechaDeTermino 
-     * @param int capacidad 
-     * @param int diasMaximoDevolucion 
-     * @param boolean publicado 
-     * @param int idPropiedad 
+     * @param nombre 
+     * @param descripcion 
+     * @param fechaDeInicio 
+     * @param fechaDeTermino 
+     * @param capacidad 
+     * @param diasMaximoDevolucion 
+     * @param publicado 
+     * @param idPropiedad 
      * @return
      */
     public boolean crearEvento(String nombre, String descripcion, Date fechaDeInicio, Date fechaDeTermino, int capacidad, int diasMaximoDevolucion, boolean publicado, int idPropiedad) {
-        // TODO implement here
-        return false;
+        boolean result = this.controlador.crearEvento(nombre, descripcion, fechaDeInicio, fechaDeTermino, capacidad, diasMaximoDevolucion, publicado, idPropiedad, descripcion);
+        if(result){
+            
+            //crear evento cuando este implementada la clase evento.
+            //Evento e = new Evento();
+            //this.listaEventos.add(e);            
+        }        
+        return result;
     }
 
     /**
-     * @param int idEvento 
-     * @param String nombre 
-     * @param String descripcion 
-     * @param Date fechaDeInicio 
-     * @param Date fechaDeTermino 
-     * @param int capacidad 
-     * @param int diasMaximoDevolucion 
-     * @param boolean publicado 
-     * @param int idPropiedad 
+     * @param idEvento 
+     * @param nombre 
+     * @param descripcion 
+     * @param fechaDeInicio 
+     * @param fechaDeTermino 
+     * @param capacidad 
+     * @param diasMaximoDevolucion 
+     * @param publicado 
+     * @param idPropiedad 
      * @return
      */
     public boolean modificarEvento(int idEvento, String nombre, String descripcion, Date fechaDeInicio, Date fechaDeTermino, int capacidad, int diasMaximoDevolucion, boolean publicado, int idPropiedad) {
-        // TODO implement here
-        return false;
+        boolean result = this.controlador.modificarEvento(idEvento, nombre, descripcion, fechaDeInicio, fechaDeTermino, capacidad, diasMaximoDevolucion, publicado, idPropiedad);
+        if(result){
+            //modificar evento cuando este implementada la clase evento
+        }                
+        return result;
     }
 
     /**
-     * @param int idEvento 
+     * Metodo que eliminar 
+     * @param idEvento 
      * @return
      */
     public boolean eliminarEvento(int idEvento) {
-        // TODO implement here
+        boolean result = this.controlador.eliminarEvento(idEvento);
+        if(result){
+            for(Evento e:this.listaEventos){
+                //buscar el id, despues eliminar evento.
+            }
+        }
         return false;
     }
 
     /**
-     * @param int idEvento 
+     * @param idEvento 
      * @return
      */
     public boolean aceptarSolicitud(int idEvento) {
-        // TODO implement here
+        this.controlador.aceptarSolicitudPropietario(idEvento);
+        
+        
         return false;
     }
 
     /**
-     * @param int idEvento 
+     * @param idEvento 
      * @return
      */
     public boolean rechazarSolicitud(int idEvento) {
-        // TODO implement here
-        return false;
+        return this.controlador.eliminarEvento(idEvento);                                
     }
 
     /**
-     * @param String rut 
-     * @param String opcion 
+     * 
+     * 
+     * @param rut 
+     * @param opcion 
      * @return
      */
     public ArrayList<Evento> obtenerInformacion(String rut, String opcion) {
