cb93d7499cfa6d8e1855a76bed7e42315e7b27a3
Unknown <felipe.milla.calquin@gmail.com>
2019-10-15 14:41:00 -0300

    Arreglo testing v1
    this

--- EvenTinder/test/VistasSistema/VistaOrganizador/TestPanelEliminarOrganiador.java
+++ EvenTinder/test/VistasSistema/VistaOrganizador/TestPanelEliminarOrganiador.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package VistaSistemaVistaOrganizador;
+package VistasSistema.VistaOrganizador;;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import VistasSistema.VistaOrganizador.VentanaPrincipalOrganizador;
