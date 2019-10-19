85fc495b889be69bf36269ae7ec0bc193f00f863
Matias <pinkystylee98@gmail.com>
2019-10-18 22:03:02 -0300

    Gestion y Controlador de Ventas/Clientes
    wololoooo

--- EvenTinder/src/ModuloGestionVentas/Compra.java
+++ EvenTinder/src/ModuloGestionVentas/Compra.java
@@ -18,6 +18,42 @@ public class Compra {
     private Date fechaDeCompra;
     private int precioTotalDeLaCompra;
 
+    public Compra(int id, int numeroDeEntrada, Date fechaDeCompra, int precioTotalDeLaCompra) {
+        this.id = id;
+        this.numeroDeEntrada = numeroDeEntrada;
+        this.fechaDeCompra = fechaDeCompra;
+        this.precioTotalDeLaCompra = precioTotalDeLaCompra;
+    }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNumeroDeEntrada() {
+        return numeroDeEntrada;
+    }
+
+    public void setNumeroDeEntrada(int numeroDeEntrada) {
+        this.numeroDeEntrada = numeroDeEntrada;
+    }
+
+    public Date getFechaDeCompra() {
+        return fechaDeCompra;
+    }
+
+    public void setFechaDeCompra(Date fechaDeCompra) {
+        this.fechaDeCompra = fechaDeCompra;
+    }
+
+    public int getPrecioTotalDeLaCompra() {
+        return precioTotalDeLaCompra;
+    }
+
+    public void setPrecioTotalDeLaCompra(int precioTotalDeLaCompra) {
+        this.precioTotalDeLaCompra = precioTotalDeLaCompra;
+    }       
 }
