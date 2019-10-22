34fa6cdcdca64ec92e5382bc70d0e4fd48422f79
Brayan <brayan.escobar@live.com>
2019-10-21 22:16:48 -0300

    Test clases del pinky

--- EvenTinder/test/ModuloGestionUsuario/GestionDeUsuarioTest.java
+++ EvenTinder/test/ModuloGestionUsuario/GestionDeUsuarioTest.java
@@ -86,7 +86,7 @@ public class GestionDeUsuarioTest {
         boolean result = instance.eliminarUsuario(tipoUsuario, rutUsuario);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+       // fail("The test case is a prototype.");
     }
 
 }
